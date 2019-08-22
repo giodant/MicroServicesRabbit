9f1e7e60b3886e74e917943bbd0839b08bfa0cf7
Giovanni D'Antonio <giovanni.dantonio@mitrol.it>
2019-08-22 17:20:42 +0200

    Implemented Interfaces and classes for Domain.Core

---
+++ MicroServicesRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,15 @@
+﻿using MicroServicesRabbit.Domain.Core.Commands;
+using MicroServicesRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroServicesRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler;
+    }
+}
---
+++ MicroServicesRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using MicroServicesRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroServicesRabbit.Domain.Core.Commands
+{
+    public abstract class Command: Message
+    {
+        public DateTime PublishDate { get; protected set; }
+
+        protected Command()
+        {
+            PublishDate = DateTime.Now;
+        }
+    }
+}
